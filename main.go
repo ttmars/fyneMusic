836d70d45a467b6f819cb343166b9da403f37f01
liwen <valar01@163.com>
2022-09-02 00:44:14 +0800

    添加自定义主题

--- main.go
+++ main.go
@@ -3,32 +3,28 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyneMusic/myTheme"
 	"fyneMusic/myWidget"
-	"os"
 )
-
-func init()  {
-	os.Setenv("FYNE_FONT", "./static/font/simkai.ttf")		// 字体设置
-}
 
 func main()  {
 	myApp := app.NewWithID("io.fyne.demo")			// 创建APP
 	myWindow := myApp.NewWindow("music")			// 创建窗口
 
-	myApp.SetIcon(theme.FyneLogo())						// 设置APP图标
+	myApp.SetIcon(myTheme.ResourceLogoJpg)			    // 设置logo
+	myApp.Settings().SetTheme(&myTheme.MyTheme{})		// 设置APP主题，嵌入字体，解决乱码
 	myWindow.Resize(fyne.NewSize(1200,800))		// 设置窗口大小
 	myWindow.CenterOnScreen()							// 窗口居中显示
 	myWindow.SetMaster()								// 设置为主窗口
-
-	//myWindow.SetCloseIntercept(func() {myWindow.Hide()})	// 设置窗口托盘显示
-	//if desk, ok := myApp.(desktop.App); ok {
-	//	m := fyne.NewMenu("MyApp",
-	//		fyne.NewMenuItem("Show", func() {
-	//			myWindow.Show()
-	//		}))
-	//	desk.SetSystemTrayMenu(m)
-	//}
+	myWindow.SetCloseIntercept(func() {myWindow.Hide()})	// 设置窗口托盘显示
+	if desk, ok := myApp.(desktop.App); ok {
+		m := fyne.NewMenu("MyApp",
+			fyne.NewMenuItem("Show", func() {
+				myWindow.Show()
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
 
 	myWindow.SetMainMenu(myWidget.MakeMyMenu(myApp, myWindow))		// 创建菜单
 	myWindow.SetContent(myWidget.MakeNav(myApp, myWindow))			// 创建导航
